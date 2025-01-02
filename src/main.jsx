2faa3444c08a79ea514831ca0b4c27ed55929375
Hemant Bhujang <130651006+HemantBhujang@users.noreply.github.com>
2025-01-02 15:27:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default defineConfig({
+  plugins: [react()],
+  base: '/Currency-Convertor/', // Replace with your repository name
+});
